b36b044b2035489b14b9595a9abfff3cdce741ed
canine89 <hgpark@goldenrabbit.co.kr>
2026-09-21 16:21:27 +0900

    release: v1.0.93 (appcast 갱신)

--- Casks/oh-my-opensnap.rb
+++ Casks/oh-my-opensnap.rb
@@ -1,6 +1,6 @@
 cask "oh-my-opensnap" do
-  version "1.0.92"
-  sha256 "dd0beaee2f42a023b07ea4611edd0721b2677e5926576b68eaf87f76641cca66"
+  version "1.0.93"
+  sha256 "7f640d45cc3922d6aecfc325aed8fb286d7fa1bbad1dbb0966882b8cba2b3d23"
 
   url "https://github.com/Canine89/oh-my-opensnap/releases/download/v#{version}/oh-my-opensnap-#{version}.dmg"
   name "oh-my-opensnap"
